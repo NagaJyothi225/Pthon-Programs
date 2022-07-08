717594a44bf908e2d75e68358fd1aa47da3a1e1c
githubjyothi <108788541+githubjyothi@users.noreply.github.com>
2022-07-08 18:29:57 +0530

    Create 7.py
    student avg marks

---
+++ 7.py
@@ -0,0 +1,8 @@
+a=int(input("enter student marks:"))
+if a<100 and a>0:
+    print("student marks 100 below")
+    if a<80:
+        print("student marks")
+        if a>40:
+            print("student marks 40")
+
