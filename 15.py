62a5b314fc9888b036c932c726bbf6a251c3252e
githubjyothi <108788541+githubjyothi@users.noreply.github.com>
2022-07-18 19:08:02 +0530

    Create 15.py
    fahrenheit covert to celsius and kevlin

---
+++ 15.py
@@ -0,0 +1,6 @@
+fh=int(input("Enter temparature in degrees fahrentheit:"))
+celsius=(fh-32)*5/9
+kelvin=(fh-32)*5/9+273.15
+print("temperature is convert to:", celsius)
+print("temperature is convert to:", kelvin)
+ 
