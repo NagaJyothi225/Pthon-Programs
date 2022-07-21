4a4781a9986398c1f58ade91391ec908c58b490c
githubjyothi <108788541+githubjyothi@users.noreply.github.com>
2022-07-21 18:20:03 +0530

    Create 20.py
    Factorial numbers

---
+++ 20.py
@@ -0,0 +1,12 @@
+num=int(input("enter any number:"))
+fact=1
+for i in range(1,num+1):
+   fact=fact*i
+print(fact)   
+
+# another method
+num=int(input("enter any number:"))
+fact=1
+for i in range(1,num+1):
+   fact=fact*i
+   print(fact)   
