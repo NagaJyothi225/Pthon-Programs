1491679a803b5a5bc034bc39b42c6e3b653dc32e
githubjyothi <108788541+githubjyothi@users.noreply.github.com>
2022-09-08 19:36:34 +0530

    Create list iterable while and for loop.py
    Writing a program list iterable object using while and for loop.

---
+++ list iterable while and for loop.py
@@ -0,0 +1,18 @@
+#list is an iterable object:
+a=[1,2,3,4,5,6]
+i=0
+while i<=5:
+    print(i)
+    i=i+1
+    
+#list is an iterable reverse object:
+b=[1,2,3,4,5,6]
+i=5
+while i>=0:
+    print(b[i])
+    i=i-1
+
+#loop iterable object;
+c=[10,20,30,40,50]
+for i in c:
+    print(i)
