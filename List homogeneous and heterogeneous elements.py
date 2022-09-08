5c6094b6cf17397ee33baa6994ad65d491d16af2
githubjyothi <108788541+githubjyothi@users.noreply.github.com>
2022-09-08 18:48:01 +0530

    Create List homogeneous and heterogeneous elements.py
    Writing a program to print list is homogeneous and heterogeneous elements.

---
+++ List homogeneous and heterogeneous elements.py
@@ -0,0 +1,7 @@
+#list is homogeneous elements:-it allowes only numbers and duplicate values also
+a=[10,20,30,40,50,10]
+print(a)
+#list is heterogeneous elements:-it allowes(int,float,complex,boolean,str)
+b=[1,'abc',(2+5j),22.5,'True']
+print(b)
+
