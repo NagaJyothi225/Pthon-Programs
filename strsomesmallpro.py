7f8e1b80d8723ed838c9752cfad032ee9bd0cd5e
githubjyothi <108788541+githubjyothi@users.noreply.github.com>
2022-09-08 10:42:03 +0530

    Create strsomesmallpro.py
    Writing a string programs ,iterating, indexing, slicing string programs it basic programs string

---
+++ strsomesmallpro.py
@@ -0,0 +1,41 @@
+#iterating string using for loop:
+for i in "naga":
+    print(i)
+a='jyothi'
+for i in a:
+    print(i)
+    
+#same question by using while loop:
+a="bujji"
+i=0
+while i<5:
+    print(a[i])
+    i=i+1
+##string indexing:
+a='string'
+print(a[0])
+print(a[-2])
+###string indexing value
+a='indexing'
+for i in range(0,8):
+    print(a[i])
+for j in range(-1,-8,-1):
+    print(a[j])
+#string slicing operation while loop:
+a="slicing"
+i=0
+while i<6:
+    print(a[i])
+    i=i+1
+###########
+b="while"
+i=-1
+while i>=-5:
+    print(b[i])
+    i=i-1
+##while slicing:
+a='indra'
+i=4
+while i>=0:
+    print(a[i])
+    i=i-1
