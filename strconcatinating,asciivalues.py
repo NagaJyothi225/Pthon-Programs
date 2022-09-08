d69caf8c80644be62d414a7a1c957e595ff5873a
githubjyothi <108788541+githubjyothi@users.noreply.github.com>
2022-09-08 10:58:01 +0530

    Create strconcatinating,asciivalues.py
    Writing a program to print adding two strings and concatenating, ASCII values and multiplying string

---
+++ strconcatinating,asciivalues.py
@@ -0,0 +1,31 @@
+###string concatinating string means two strings adding:
+a='jyothi'
+b='nagaraju'
+c=a+b
+print(c)
+###appending string:- means adding one variable to another variable thatis append
+a="naga"
+b=a+'jyothi'
+print(b)
+
+##Multiplaying string using(*)
+a='amma'
+print(a*2)
+#    Or   #
+a='Amma'
+b=5
+print(a*b)
+'''##String and adding number:
+a='NAnna'
+b=5
+#it is error comes because string and integer values not adding
+print(a+b)'''
+
+#Comparing two string ASCII values:
+a='A'
+b='a'
+print(b>a)
+print(b<a)
+print(ord(a))
+print(ord(b))
+
